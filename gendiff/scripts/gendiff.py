02835b1bcc587d55892b6af891e03bb6dc247f80
Denis Bolshakov <dreu007@gmail.com>
2023-03-02 11:31:11 +0400

    Feat gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,8 +11,11 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
+
     args = parser.parse_args()
-    print(args.accumulate())
+    print(args.accumulate(args.first_file, args.second_file))
 
 
 if __name__ == "__main__":

16fcdd7f16d1f7e44878c58cbbbd1b30d0f20e30
Denis Bolshakov <dreu007@gmail.com>
2023-03-04 21:41:14 +0400

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -16,24 +16,22 @@ def parse_cli():
                         help='set format of output')
 
     args = parser.parse_args()
-    print(args.accumulate(args.first_file, args.second_file))
-
-
-def bool_to_json(value):
-    decoder = {True: 'true',
-               False: 'false',
-               None: 'null'}
-    return decoder.get(value, value)
+    # print(args.accumulate(args.first_file, args.second_file))
+    return args.first_file, args.second_file, args.format
 
 
 def generate_diff(file_path1, file_path2):
+    decoder = {True: 'true',
+               False: 'false',
+               None: 'null'}
+
     with open(file_path1, 'r') as file1:
         dict1 = json.load(file1)
-        data1 = {key: bool_to_json(value) for key, value in dict1.items()}
+        data1 = {key: decoder.get(val, val) for key, val in dict1.items()}
 
     with open(file_path2, 'r') as file2:
         dict2 = json.load(file2)
-        data2 = {key: bool_to_json(value) for key, value in dict2.items()}
+        data2 = {key: decoder.get(val, val) for key, val in dict2.items()}
 
     all_keys = sorted(set(data1.keys()) | set(data2.keys()))
 
@@ -57,5 +55,11 @@ def generate_diff(file_path1, file_path2):
     return result
 
 
+def main():
+    first, second, format_data = parse_cli()
+    if format_data is None:  # TODO: update None to type of format_data
+        print(generate_diff(first, second))
+
+
 if __name__ == "__main__":
-    parse_cli()
+    main()

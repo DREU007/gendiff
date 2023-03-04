42afb37ed984a4d339b0c6737ec3cb65aca72f57
Denis Bolshakov <dreu007@gmail.com>
2023-03-04 14:31:45 +0400

    Feat gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
 
-def main():
+def parse_cli():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.'
@@ -18,5 +19,42 @@ def main():
     print(args.accumulate(args.first_file, args.second_file))
 
 
+# def preserve_bool(value):
+    # return value if value in {'true', 'false', 'null'} else None
+# def preserve_bool(val):
+#     if val == 'true':
+#         return val
+#     elif val == 'false':
+#         return val
+#     else:
+#         return None  # Return None to let JSON decoder handle the value.
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+
+    result = '{\n'
+    for key in all_keys:
+        first = data1.get(key)
+        second = data2.get(key)
+
+        first_msg = f'- {key}: {first}\n'
+        second_msg = f'+ {key}: {second}\n'
+
+        if first and second:
+            if first == second:
+                result += f'  {key}: {first}\n'
+            else:
+                result += first_msg + second_msg
+        else:
+            result += first_msg if first else second_msg
+
+    result += '}'
+    return result
+
+
 if __name__ == "__main__":
-    main()
+    parse_cli()

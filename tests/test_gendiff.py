af2d613b93b0ab9fa8bda30e47ee1f929acef7e7
Denis Bolshakov <dreu007@gmail.com>
2023-03-06 15:18:43 +0400

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,12 +1,21 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-filepath1 = 'tests/fixtures/file1.json'
-filepath2 = 'tests/fixtures/file2.json'
+json1_path = 'tests/fixtures/file1.json'
+json2_path = 'tests/fixtures/file2.json'
 
-flat_result_path = "tests/fixtures/flat_json_result.txt"
+yaml1_path = 'tests/fixtures/file1.yml'
+yaml2_path = 'tests/fixtures/file2.yml'
+
+flat_json_result_path = "tests/fixtures/flat_json_result.txt"
+flat_yaml_result_path = "tests/fixtures/flat_yaml_result.txt"
 
 
-def test_basic_json():
-    with open(flat_result_path, 'r') as flat_result:
-        assert generate_diff(filepath1, filepath2) == flat_result.read()
+def test_flat_json():
+    with open(flat_json_result_path, 'r') as flat_json_result:
+        assert generate_diff(json1_path, json2_path) == flat_json_result.read()
+
+
+def test_flat_yaml():
+    with open(flat_yaml_result_path, 'r') as flat_yaml_result:
+        assert generate_diff(yaml1_path, yaml2_path) == flat_yaml_result.read()

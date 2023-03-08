9617d13361b73aaf9bca863e5c20a1a5ac03a392
Denis Bolshakov <dreu007@gmail.com>
2023-03-08 13:02:13 +0400

    Feat gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python3
-import argparse
 import json
 import yaml
-
-
-def parse_cli():
-    """Parse command line args and/ or returns tuple filepaths 1, 2 & format.
-
-    :returns: (filepath1, filepath2, format). Tuple of filepath to first_file,
-    second_file and format if any, else default."""
-
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        description='Compares two configuration files and shows a difference.'
-    )
-
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-
-    parser.add_argument('-f', '--format',
-                        help='set format of output')
-
-    args = parser.parse_args()
-    # print(args.accumulate(args.first_file, args.second_file))
-    return args.first_file, args.second_file, args.format
+from gendiff.parse_cli import parse_cli
 
 
 JSON_DECODER = {True: 'true',
@@ -38,9 +16,28 @@ def get_json_dict(file_path):
         return data
 
 
-def generate_diff(file_path1, file_path2):
-    data1 = get_json_dict(file_path1)
-    data2 = get_json_dict(file_path2)
+def get_yaml_dict(file_path):
+    with open(file_path, 'r') as file:
+        _dict = yaml.safe_load(file)
+        # TODO: Fix JSON_DECODER in YAML func
+        data = {key: JSON_DECODER.get(val, val) for key, val in _dict.items()}
+        print(_dict)
+        return data
+
+
+def generate_diff(filepath1: str, filepath2: str):
+    extension_a = filepath1.rsplit('.', 1)[-1]
+    extension_b = filepath2.rsplit('.', 1)[-1]
+
+    # Check type of file
+    if extension_a == extension_b == 'json':
+        data1 = get_json_dict(filepath1)
+        data2 = get_json_dict(filepath2)
+    elif extension_a == extension_b == 'yml':
+        data1 = get_yaml_dict(filepath1)
+        data2 = get_yaml_dict(filepath2)
+    else:
+        raise TypeError("Error Different FileTypes")
 
     all_keys = sorted(set(data1.keys()) | set(data2.keys()))
 
@@ -49,12 +46,12 @@ def generate_diff(file_path1, file_path2):
         first = data1.get(key)
         second = data2.get(key)
 
-        first_msg = f'- {key}: {first}\n'
-        second_msg = f'+ {key}: {second}\n'
+        first_msg = f'  - {key}: {first}\n'
+        second_msg = f'  + {key}: {second}\n'
 
         if first and second:
             if first == second:
-                result += f'  {key}: {first}\n'
+                result += f'{" " * 4}{key}: {first}\n'
             else:
                 result += first_msg + second_msg
         else:

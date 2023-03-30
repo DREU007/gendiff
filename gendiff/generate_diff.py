262c66b8a1f6d3b17ddda0708b560d32d5328d97
Denis Bolshakov <dreu007@gmail.com>
2023-03-30 05:33:32 +0400

    Add generate_diff.py

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,19 @@
+from gendiff.parse_data import make_diff, get_data
+from gendiff.formats.format_stylish import stringify
+from gendiff.formats.format_plain import make_plain
+from gendiff.formats.format_json import make_json
+
+
+def generate_diff(filepath1: str, filepath2: str, format_data='stylish'):
+    data1 = get_data(filepath1)
+    data2 = get_data(filepath2)
+    diff = make_diff(data1, data2)
+    if format_data == 'stylish':
+        return stringify(diff)
+    elif format_data == 'plain':
+        return make_plain(diff)
+    elif format_data == 'json':
+        # with open('diff.json', 'w') as file:
+        #     diff = make_json(diff)
+        #     json.dump(diff, file)
+        return make_json(diff)

c1f5ba008bf7ae055a97112a121a1e7db7295733
Denis Bolshakov <dreu007@gmail.com>
2023-03-30 05:41:02 +0400

    Update __init__.py

---
+++ gendiff/formats/__init__.py
@@ -0,0 +1,10 @@
+from gendiff.formats.format_stylish import stringify
+from gendiff.formats.format_plain import make_plain
+from gendiff.formats.format_json import make_json
+
+
+__all__ = (
+    'stringify',
+    'make_plain',
+    'make_json'
+)

783876351d1ecef09818d5ca52acadc0d7a63ead
Denis Bolshakov <dreu007@gmail.com>
2023-04-02 15:32:29 +0400

    Refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,40 +1,20 @@
-from gendiff.scripts.gendiff import generate_diff
+import os
 import json
+from gendiff import generate_diff
 
 
-json1_tree_path = 'tests/fixtures/file_tree1.json'
-json2_tree_path = 'tests/fixtures/file_tree2.json'
-tree_json_result_path = "tests/fixtures/tree_json_result.txt"
+def test_generate_diff(gendiff_fixtures):
+    before_fp, after_fp, result_fp, format = gendiff_fixtures
+    paths = {"before": before_fp, "after": after_fp, "result": result_fp}
 
+    for key, value in paths.items():
+        relative_fp = 'fixtures/' + value
+        paths[key] = os.path.join(os.path.dirname(__file__), relative_fp)
 
-def test_tree_json():
-    with open(tree_json_result_path, 'r') as result:
-        assert generate_diff(json1_tree_path, json2_tree_path) == result.read()
-
-
-yaml1_tree_path = 'tests/fixtures/file_tree1.yml'
-yaml2_tree_path = 'tests/fixtures/file_tree2.yml'
-tree_yaml_result_path = "tests/fixtures/tree_yaml_result.txt"
-
-
-def test_tree_yaml():
-    with open(tree_yaml_result_path, 'r') as result:
-        assert generate_diff(yaml1_tree_path, yaml2_tree_path) == result.read()
-
-
-plain_json_result = "tests/fixtures/plain_json_result.txt"
-
-
-def test_plain_json():
-    with open(plain_json_result, 'r') as result:
-        assert generate_diff(json1_tree_path, json2_tree_path, "plain"
-                             ) == result.read()
-
-
-json_format_result_path = 'tests/fixtures/json_format_result.json'
-
-
-def test_json_format():
-    with open(json_format_result_path, 'r') as result:
-        data = generate_diff(json1_tree_path, json2_tree_path, "json")
-        assert json.loads(data) == json.load(result)
+    with open(paths["result"], 'r') as result:
+        if format != "json":
+            assert generate_diff(
+                paths["before"], paths["after"], format) == result.read()
+        else:
+            data = generate_diff(paths["before"], paths["after"], format)
+            assert json.loads(data) == json.load(result)

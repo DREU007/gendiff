e4a20080f93b887abda97811b92f7ce59b1eedfb
Denis Bolshakov <dreu007@gmail.com>
2023-04-11 17:10:04 +0400

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,39 @@
 import os
 import json
+import pytest
 from gendiff import generate_diff
 
 
-def test_generate_diff(gendiff_fixtures):
-    before_fp, after_fp, result_fp, format = gendiff_fixtures
+@pytest.mark.parametrize(argnames="prepared_files",
+                         argvalues=[
+                             (
+                                'tree1.json',
+                                'tree2.json',
+                                "tree_json_result.txt",
+                                "stylish"
+                             ),
+                             (
+                                'tree1.yml',
+                                'tree2.yml', "tree_yaml_result.txt",
+                                "stylish"
+                             ),
+                             (
+                                 'tree1.json',
+                                 'tree2.json',
+                                 "plain_json_result.txt",
+                                 "plain"
+                             ),
+                             (
+                                 'tree1.json',
+                                 'tree2.json',
+                                 "json_format_result.json",
+                                 "json"
+                             ),
+                         ],
+                         # indirect=True
+                         )
+def test_generate_diff(prepared_files):
+    before_fp, after_fp, result_fp, _format = prepared_files
     paths = {"before": before_fp, "after": after_fp, "result": result_fp}
 
     for key, value in paths.items():
@@ -12,9 +41,9 @@ def test_generate_diff(gendiff_fixtures):
         paths[key] = os.path.join(os.path.dirname(__file__), relative_fp)
 
     with open(paths["result"], 'r') as result:
-        if format != "json":
+        if _format != "json":
             assert generate_diff(
-                paths["before"], paths["after"], format) == result.read()
+                paths["before"], paths["after"], _format) == result.read()
         else:
-            data = generate_diff(paths["before"], paths["after"], format)
+            data = generate_diff(paths["before"], paths["after"], _format)
             assert json.loads(data) == json.load(result)

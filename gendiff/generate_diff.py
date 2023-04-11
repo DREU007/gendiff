b2de3e4d5e9129db9e943cfc869e995d170291fa
Denis Bolshakov <dreu007@gmail.com>
2023-04-11 20:41:43 +0400

    Refactor! parse_data.py generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,17 +1,10 @@
-import json
-from gendiff.parse_data import make_diff, get_data
-from gendiff.formats import stringify
-from gendiff.formats import make_plain
-from gendiff.formats import make_json
+from gendiff.parse_data import get_data, parse_data
+from gendiff.diff_tree import make_diff
+from gendiff.formatting import get_format
 
 
 def generate_diff(filepath1: str, filepath2: str, format_data='stylish'):
-    data1 = get_data(filepath1)
-    data2 = get_data(filepath2)
+    data1 = parse_data(*get_data(filepath1))
+    data2 = parse_data(*get_data(filepath2))
     diff = make_diff(data1, data2)
-    if format_data == 'stylish':
-        return stringify(diff)
-    elif format_data == 'plain':
-        return make_plain(diff)
-    elif format_data == 'json':
-        return json.dumps(make_json(diff))
+    return get_format(diff, format_data)

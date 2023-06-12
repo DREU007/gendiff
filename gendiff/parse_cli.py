04e66fb87deb883d30fc6cbe4d42ea85a38e5c4b
Denis Bolshakov <dreu007@gmail.com>
2023-06-12 10:00:41 +0400

    Update parse_cli.py

--- gendiff/parse_cli.py
+++ gendiff/parse_cli.py
@@ -7,16 +7,22 @@ def parse_cli():
     :returns: (filepath1, filepath2, format). Tuple of filepath to first_file,
     second_file and format if any, else default."""
 
+    available_formats = ['stylish', 'plain', 'json']
+
     parser = argparse.ArgumentParser(
         prog='gendiff',
+        formatter_class=argparse.RawTextHelpFormatter,
         description='Compares two configuration files and shows a difference.'
     )
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
-    parser.add_argument('-f', '--format', default='stylish',
-                        help='set format of output')
+    parser.add_argument(
+        '-f', '--format', default='stylish',
+        help=f'''set format of output (default: %(default)s).
+Formats: {{{", ".join(available_formats)}}}.'''
+    )
 
     args = parser.parse_args()
     return args.first_file, args.second_file, args.format
